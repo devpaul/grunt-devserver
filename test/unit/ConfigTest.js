272cec026b1f3328b2c63eccebe8a054a3ae2d52
devpaul <git@devpaul.com>
2013-04-04 09:43:30 -0700

    Adding cacheControl configuration options to Config

--- test/unit/ConfigTest.js
+++ test/unit/ConfigTest.js
@@ -10,6 +10,7 @@ describe('ConfigTest', function() {
         it('is prototyped correctly', function() {
             expect(Config.DEFAULT_PORT).to.be.equal(8888)
             expect(Config.DEFAULT_FOLDER).to.be.equal(process.cwd())
+            expect(Config.DEFAULT_CACHE_CONTROL).to.be.equal('no-cache')
         })
 
         it('supports functional construction', function() {
@@ -23,6 +24,7 @@ describe('ConfigTest', function() {
             expect(config).to.be.an.instanceof(Config)
             expect(config.port).to.be.equal(Config.DEFAULT_PORT)
             expect(config.folder).to.be.equal(Config.DEFAULT_FOLDER)
+            expect(config.cacheControl).to.be.equal(Config.DEFAULT_CACHE_CONTROL)
         }
     })
 });
