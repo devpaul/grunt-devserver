704a0a830cccec033cdfb596df995b587b790926
pshannon <pshannon@cyberitas.com>
2013-07-13 10:23:08 -0700

    uploaded configuration loading to match business rules and validates options file structure

--- test/unit/commands/loadCompleteOptionsCmdTest.js
+++ test/unit/commands/loadCompleteOptionsCmdTest.js
@@ -13,14 +13,18 @@ describe('loadCompleteOptionsCmdTest', function() {
             loadCompleteOptionsCmd = SandboxedModule.require(UNIT_UNDER_TEST_PATH, { requires: requires })
     }
 
-    it('throws when a file is missing', function() {
+    it('returns the passed options when no file load is present', function() {
+        var options = {}
+          , actual
+
         setupUnitUnderTest()
-        expect(loadCompleteOptionsCmd).to.throw()
+        actual = loadCompleteOptionsCmd(options)
+        expect(options).to.be.equal(actual)
     })
 
     it('loads one option file without a file reference', function() {
         var options = { type: 'http' }
-          , expected = options
+          , expected = { type: 'http' }
 
         setupUnitUnderTest()
 
@@ -32,39 +36,56 @@ describe('loadCompleteOptionsCmdTest', function() {
           , options = { type: 'http'
                       , file: filename
                       }
-          , firstOption = { type: 'https'
-                          , cache: 'no-cache'
-                          }
+          , optionFile = { options: { type: 'https'
+                                    , cache: 'no-cache'
+                                    }
+                         }
           , expected = { type: 'http'
                        , cache: 'no-cache'
                        , file: filename
                        }
           , files = {}
 
-        files[path.resolve(filename)] = firstOption
+        files[path.resolve(filename)] = optionFile
         setupUnitUnderTest(files)
 
         expect(loadCompleteOptionsCmd(options)).to.deep.equal(expected)
     })
 
-    it('loads an option file with many chained references', function() {
+    it('throws on a malformed option configuration file', function() {
+        var filename = 'first'
+          , options = { type: 'http'
+                      , file: filename
+                      }
+          , optionFile = {}
+          , files = {}
+
+        files[path.resolve(filename)] = optionFile
+        setupUnitUnderTest(files)
+
+        expect(function() {
+            loadCompleteOptionsCmd(options)
+        }).to.throw()
+    })
+
+    it('does not support loading recursive configuration files', function() {
         var firstFilename = 'first'
           , secondFilename = 'second'
           , options = { type: 'http'
                       , file: firstFilename
                       }
-          , firstOptions = { type: 'https'
-                           , file: secondFilename
-                           }
-          , secondOptions = { cacheControl: 'no-cache' }
+          , firstFile = { options: { type: 'https'
+                                   , file: secondFilename
+                                   }
+                        }
+          , secondFile = { cacheControl: 'no-cache' }
           , expected = { type: 'http'
-                       , cacheControl: 'no-cache'
                        , file: firstFilename
                        }
           , files = {}
 
-        files[path.resolve(firstFilename)] = firstOptions
-        files[path.resolve(secondFilename)] = secondOptions
+        files[path.resolve(firstFilename)] = firstFile
+        files[path.resolve(secondFilename)] = secondFile
         setupUnitUnderTest(files)
 
         expect(loadCompleteOptionsCmd(options)).to.deep.equal(expected)
