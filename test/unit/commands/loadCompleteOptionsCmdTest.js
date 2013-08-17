d9a874becf79c89028f175f7422fb121ed4e1051
Paul Shannon <a@devpaul.com>
2013-08-16 21:32:26 -0700

    Replacing Config with CompositeOptions

--- test/unit/commands/loadCompleteOptionsCmdTest.js
+++ test/unit/commands/loadCompleteOptionsCmdTest.js
@@ -1,106 +1,58 @@
-var SandboxedModule = require('sandboxed-module')
-  , path = require('path')
+var loadCompleteOptions = require('../../../lib/commands/loadCompleteOptionsCmd.js')
+  , BasicOptions = require('../../../lib/model/BasicOptions.js')
+  , MultiOptions = require('../../../lib/model/MultiOptions.js')
+  , CompositeOptions = require('../../../lib/model/CompositeOptions.js')
 
 describe('loadCompleteOptionsCmdTest', function() {
-    var loadCompleteOptionsCmd
+    var loadOptionsFileStub
 
-    function setupUnitUnderTest(requires) {
-        var UNIT_UNDER_TEST_PATH = '../../../lib/commands/loadCompleteOptionsCmd.js'
-
-        if(!requires)
-            loadCompleteOptionsCmd = require(UNIT_UNDER_TEST_PATH)
-        else
-            loadCompleteOptionsCmd = SandboxedModule.require(UNIT_UNDER_TEST_PATH, { requires: requires })
-    }
-
-    it('returns the passed options when no file load is present', function() {
-        var options = {}
-          , actual
-
-        setupUnitUnderTest()
-        actual = loadCompleteOptionsCmd(options)
-        expect(options).to.be.equal(actual)
+    beforeEach(function() {
+        loadOptionsFileStub = sinon.stub(MultiOptions.prototype, '_loadOptionsFile')
     })
 
-    it('loads one option file without a file reference', function() {
-        var options = { type: 'http' }
-          , expected = { type: 'http' }
-
-        setupUnitUnderTest()
-
-        expect(loadCompleteOptionsCmd(options)).to.deep.equal(expected)
+    afterEach(function() {
+        loadOptionsFileStub.restore()
     })
 
-    it('loads an option file with a file reference', function() {
-        var filename = 'first'
-          , options = { type: 'http'
-                      , file: filename
-                      }
-          , optionFile = { options: { type: 'https'
-                                    , cache: 'no-cache'
-                                    }
-                         }
-          , expected = { type: 'http'
-                       , cache: 'no-cache'
-                       , file: filename
-                       }
-          , files = {}
-
-        files[path.resolve(filename)] = optionFile
-        setupUnitUnderTest(files)
-
-        expect(loadCompleteOptionsCmd(options)).to.deep.equal(expected)
-    })
-
-    it('throws on a malformed option configuration file', function() {
-        var filename = 'first'
-          , options = { type: 'http'
-                      , file: filename
-                      }
-          , optionFile = {}
-          , files = {}
-
-        files[path.resolve(filename)] = optionFile
-        setupUnitUnderTest(files)
-
+    it('throws when options is falsy (null)', function() {
         expect(function() {
-            loadCompleteOptionsCmd(options)
+            loadCompleteOptions()
         }).to.throw()
     })
 
-    it('does not support loading recursive configuration files', function() {
-        var firstFilename = 'first'
-          , secondFilename = 'second'
-          , options = { type: 'http'
-                      , file: firstFilename
-                      }
-          , firstFile = { options: { type: 'https'
-                                   , file: secondFilename
-                                   }
-                        }
-          , secondFile = { cacheControl: 'no-cache' }
-          , expected = { type: 'http'
-                       , file: firstFilename
-                       }
-          , files = {}
-
-        files[path.resolve(firstFilename)] = firstFile
-        files[path.resolve(secondFilename)] = secondFile
-        setupUnitUnderTest(files)
-
-        expect(loadCompleteOptionsCmd(options)).to.deep.equal(expected)
+    it('throws when options is not an object', function() {
+        expect(function() {
+            loadCompleteOptions('not an object')
+        }).to.throw()
     })
 
-    it('throws when a load cycle is detected', function() {
-        var filename = 'first'
-          , options = { file: 'first' }
-          , files = {}
+    it('return BasicOptions when options is an object literal', function() {
+        var options = { port: 80 }
+          , actual = loadCompleteOptions(options)
 
-        files[path.resolve(filename)] = options
-        setupUnitUnderTest(files)
+        expect(actual).to.be.an.instanceof(BasicOptions)
+        expect(actual.getOptions()).to.deep.equal(options)
+    })
 
-        expect(function() {
-            loadCompleteOptionsCmd(options)
-        }).to.throw()
+    it('returns BasicOptions when options is a *Options duck-type', function() {
+        var options = new BasicOptions()
+
+        expect(loadCompleteOptions(options)).to.be.equal(options)
+    })
+
+    describe('options composites', function() {
+        beforeEach(function() {
+            loadOptionsFileStub.returns({ options: { type: 'https' }
+                                        , production: { port: 443 }
+                                       })
+        })
+
+        it('returns a composite when a file property is present', function() {
+            var options = { file: './servers.json' }
+              , actual = loadCompleteOptions(options)
+
+            expect(actual).to.be.an.instanceof(CompositeOptions)
+            expect(['production']).to.deep.equal(actual.getConfigurationNames())
+        })
     })
 })
